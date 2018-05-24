fec14514effa7b1a95b4233556d8a5864542214d
TuomasVille <tuomas_kayhty@hotmail.com>
2018-05-24 11:10:01 +0300

    Added  service Repositories and interfaces

---
+++ ContactsWebApi/ContactsWebApi/Controllers/ContactsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContactsWebApi.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/contacts")]
+    public class ContactsController : Controller
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return new JsonResult(new { firstName = "Mike", lastName = "Ross"});
+        }
+    }
+}
