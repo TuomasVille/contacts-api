c49ddcb96473c39ab0c49548b01d8023c24ea97b
TuomasVille <tuomas_kayhty@hotmail.com>
2018-05-24 14:54:05 +0300

    Created create, update and delete

--- ContactsWebApi/ContactsWebApi/Repositories/IContactRepository.cs
+++ ContactsWebApi/ContactsWebApi/Repositories/IContactRepository.cs
@@ -1,11 +1,17 @@
-﻿using System;
+﻿using ContactsWebApi.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace ContactsWebApi.Repositories
 {
-    interface IContactRepository
+    public interface IContactRepository
     {
+        List<Contact> Get();
+        Contact Get(int id);
+        Contact Create(Contact contact);
+        Contact Update(Contact contact);
+        void Delete(int id);
     }
 }
--- ContactsWebApi/ContactsWebApi/Services/IContactService.cs
+++ ContactsWebApi/ContactsWebApi/Services/IContactService.cs
@@ -1,11 +1,17 @@
-﻿using System;
+﻿using ContactsWebApi.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace ContactsWebApi.Services
 {
-    interface IContactService
+    public interface IContactService
     {
+        List<Contact> GetContacts();
+        Contact GetContactById(int id);
+        Contact CreateContact(Contact contact);
+        Contact UpdateContact(int id, Contact contact);
+        void DeleteContact(int id);
     }
 }
